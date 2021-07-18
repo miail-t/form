8a74f86b59dec6f720da24b76189ba101980dbfc
Mikhail Timkov <mikhail.timkov.adci@gmail.com>
2021-07-19 01:42:46 +0600

    final

--- index.ts
+++ index.ts
@@ -6,20 +6,20 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.json({ extended: true }));
+app.use(express.json({extended: true}));
 app.use('/api/auth', require('./routes/auth.routers'));
 app.use('/api/profile', require('./routes/user.routers'));
 
-const PORT = 8087;
+const PORT = process.env.PORT || 8087;
 
 async function start() {
     try {
         await mongoose.connect(config.get('mongoUri'), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex:true
+            useCreateIndex: true
         })
-        app.listen(PORT , () => console.log('Вроде работает'))
+        app.listen(PORT, () => console.log('Вроде работает'))
     } catch (e) {
         console.log('Server error');
         process.exit(1)
